280193af1c45e1010d2932109b601a00120a144e
harneetgagan <hardneetgagan@gmail.com>
2025-05-28 15:31:08 -0700

    Add files via upload

--- auth.js
+++ auth.js
@@ -2,13 +2,19 @@
 window.onload = function() {
     // List of pages to protect
     const protectedPages = [
+        '', // root path (for GitHub Pages)
         'index.html',
         'events.html',
         'gallery.html',
         'rsvp.html'
     ];
-    const currentPage = window.location.pathname.split('/').pop();
-    if (protectedPages.includes(currentPage) && !sessionStorage.getItem('isAuthenticated')) {
+    // Get the last part of the path, or '' for root
+    let currentPage = window.location.pathname.split('/').pop();
+    // On GitHub Pages, root path is '' (empty string)
+    if (
+        protectedPages.includes(currentPage) &&
+        !sessionStorage.getItem('isAuthenticated')
+    ) {
         window.location.href = 'login.html';
     }
 } 
